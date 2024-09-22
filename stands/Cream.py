6b9cd8700e7882b3444b5656606c3377e9de7a2b
kaska0511 <phoipon_0511@yahoo.co.jp>
2024-09-23 00:35:41 +0900

    クリームの能力調整と軽微なバグ修正。

--- stands/Cream.py
+++ stands/Cream.py
@@ -21,9 +21,10 @@ class Cream(Common_func):
                 # 右クリックした人が本人なら能力発動
                 # スペクテイターモードに変更
                 self.run_stand = True
+                self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0')
                 self.ext.extention_command(f'execute as {self.name} at @s run gamemode spectator')
                 self.effect_stand()
-        self.right_click = False
+            self.right_click = False
 
         # 能力発動処理
         if self.run_stand:
@@ -40,28 +41,31 @@ class Cream(Common_func):
             self.new_pos = self.get_pos()
             if self.old_pos == self.new_pos:   # 座標が同じ＝動いていない＝覗き見る。
                 self.clear_effect()
-                self.ext.extention_command(f'execute as {self.name} at @s run gamemode survival')           # 覗き見ている場合はダメージを受ける状態へ
                 self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0')   # サバイバル状態だけど浮いたままにする。
+                self.ext.extention_command(f'execute as {self.name} at @s run gamemode survival')           # 覗き見ている場合はダメージを受ける状態へ
+                #self.ext.extention_command(f'effect clear {self.name} minecraft:invisibility')              # 透明化解除
+
             else:   # 比較元を更新
                 self.old_pos = self.new_pos
                 self.effect_stand()
                 self.ext.extention_command(f'execute as {self.name} at @s run gamemode spectator')
-                self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0.08')
 
-        if self.left_click: # 左クリックしたら能力を解除する。
-            self.cancel_stand()
-            self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0.08')
-        self.left_click = False
+        # 自主能力解除
+        if tag == "Cream":
+            if self.left_click and self.run_stand == True: # 能力発動中に左クリックしたら能力を解除する。
+                self.cancel_stand()
+                self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0.08')
+            self.left_click = False
 
     def cancel_stand(self):
         self.run_stand = False
         self.clear_effect()
-        self.ext.extention_command(f'effect clear {self.name} minecraft:invisibility')      # 暗闇
+        #self.ext.extention_command(f'effect clear {self.name} minecraft:invisibility')
         self.ext.extention_command(f'execute as {self.name} at @s run gamemode survival')
         self.ext.extention_command(f'attribute {self.name} minecraft:generic.gravity base set 0.08')
 
     def effect_stand(self):
-        self.ext.extention_command(f'effect give {self.name} minecraft:invisibility infinite 255 true')       # 透明化
+        #self.ext.extention_command(f'effect give {self.name} minecraft:invisibility infinite 255 true')       # 透明化
         # この二つを設定することで、ほぼ視界が奪われる。（足下が少し見える程度）
         self.ext.extention_command(f'effect give {self.name} minecraft:darkness infinite 255 true')         # 暗闇
         self.ext.extention_command(f'effect give {self.name} minecraft:blindness infinite 255 true')        # 盲目
@@ -72,7 +76,7 @@ class Cream(Common_func):
 
     def damage_within_range(self):
         # 中心に近いほど大きなダメージを食らう。通常の体力の3/4を削り取る。
-        self.ext.extention_command(f'execute as @e[distance=..1.3] at @s run damage @e[distance=..1.3,name=!{self.name}] 15 minecraft:indirect_magic by {self.name}')
+        self.ext.extention_command(f'execute at {self.name} as @e[distance=..1.3,name=!{self.name}] run damage @s 15 minecraft:indirect_magic by {self.name}')
         # 外側は体力半分を持っていくダメージ量。
-        self.ext.extention_command(f'execute as @e[distance=1.3..2.3] at @s run damage @e[distance=1.3..2.3,name=!{self.name}] 10 minecraft:indirect_magic by {self.name}')
+        self.ext.extention_command(f'execute at {self.name} as @e[distance=1.3..2.3,name=!{self.name}] run damage @s 10 minecraft:indirect_magic by {self.name}')
 
