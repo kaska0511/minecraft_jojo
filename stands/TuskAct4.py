377aaedfde26f25e76e5b1938251224829a5b26d
kaska0511 <phoipon_0511@yahoo.co.jp>
2025-05-03 14:18:07 +0900

    [TuskAct4][ADJUST]killコマンドの対象からエンダードラゴンを除外

--- stands/TuskAct4.py
+++ stands/TuskAct4.py
@@ -164,7 +164,7 @@ class TuskAct4(Common_func):
         self.ext.extension_command(f'execute as @e[name=TuskAct4,limit=1] at @s if entity @e[distance=..1,type=!item,tag=!checkpoint,name=!TuskAct4] run playsound minecraft:entity.generic.explode master @a ~ ~ ~ 4')
         self.ext.extension_command(f'execute as @e[name=TuskAct4,limit=1] at @s if entity @e[distance=..1,type=!item,tag=!checkpoint,name=!TuskAct4] run damage @e[distance=..1,type=!item,tag=!checkpoint] 999999999999999999999 minecraft:explosion by {self.name}')
         # ゴールド・エクスペリエンス・レクイエムはkillしない特別措置。レクエイム化したプレイヤーにはrequiemのタグが付与されている。
-        self.ext.extension_command(f'execute as @e[name=TuskAct4,limit=1] at @s if entity @e[distance=..1,type=!item,tag=!checkpoint,name=!TuskAct4] run kill @e[distance=..1,type=!item,tag=!checkpoint,tag=!requiem]')   # ターゲットキル。接触しているものもキル。スタンド自身もキル。
+        self.ext.extension_command(f'execute as @e[name=TuskAct4,limit=1] at @s if entity @e[distance=..1,type=!item,type=!ender_dragon,tag=!checkpoint,,tag=!requiem,name=!TuskAct4] run kill @e[distance=..1,type=!item,type=!ender_dragon,tag=!checkpoint,tag=!requiem]')   # ターゲットキル。接触しているものもキル。スタンド自身もキル。
 
         # もしターゲットがいないなら処理。デスポーンやログアウト用
         result = self.ext.extension_command(f'execute unless entity @e[name=TuskAct4,limit=1] run data get entity {self.name} DeathTime') # tag=Tusk_Targetが居ないなら、スタンド使いのDeathTimeを取得する。
